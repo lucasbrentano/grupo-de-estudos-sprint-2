af38a91f21fe7a822af25b2bd2727beeb5b87f28
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-09-13 00:50:21 -0300

    organizando os dados e colocando eles em um dicionário

--- LuanPrevisaoTempo.js
+++ LuanPrevisaoTempo.js
@@ -1,58 +1,65 @@
 
 
 const apiLocationKey = "2cdadc3ccefb45fca92bb32fe6561483";
+const apiWeatherKey="e1a0783237a3420634cabacb387c1461"
 
 
 
 async function getLocation() {
     //Pegar o nome da cidade, nome do estado e nome do país do front
-    city = `Porto%Alegre%RS%Brasil`
+    city = `sao%leopoldo%rs`
     let apiLocationUrl = `https://api.geoapify.com/v1/geocode/search?text=${city}&format=json&apiKey=${apiLocationKey}`
     const resLoc = await fetch(apiLocationUrl);
     const dadosLoc = await resLoc.json();
     lat = dadosLoc.results[0].lat;
     long = dadosLoc.results[0].lon;
-
     return [lat,long];
 }
 
 
 async function getDados(lat,lon) {
-    const apiUrl=`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=e1a0783237a3420634cabacb387c1461&lang=pt_br `
-
+    const apiUrl=`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiWeatherKey}&lang=pt_br `
     const res = await fetch(apiUrl)
     const dados = await res.json()
     return dados
 }
-async function MostraTempo() {
+let dadosClimaticos = [];
+async function getDadosClima() {
         const [x,y]= await getLocation()
-        console.log(x)
-        console.log(y)
-        const data = await getDados(x,y);
-        console.log(data.city.name)
-        console.log(data.list[0].dt_txt)
-        console.log("/////////////////////")
-        console.log(`dia ${0}`)
-        console.log(`   temperatura:`, data.list[0].main.temp)
-        console.log(`   umidade:`,data.list[0].main.humidity)
-        console.log(`   clima:`,data.list[0].weather[0].main)
-        console.log(`   descrição do clima:`,data.list[0].weather[0].description)
-        console.log(`   icone:`,data.list[0].weather[0].icon)
-        console.log(`   velocidade:`,data.list[0].wind.speed)
-        console.log(`   pais:`,data.city.country)
-        data.list.map((item) => {
-            if (item.dt_txt.split(' ')[1] === "18:00:00") {
-                console.log("/////////////////////")
-                console.log(item.dt_txt)
-                console.log(`dia ${item.dt_txt}`)
-                console.log(`   temperatura:`, item.main.temp)
-                console.log(`   umidade:`,item.main.humidity)
-                console.log(`   clima:`,item.weather[0].main)
-                console.log(`   descrição do clima:`,item.weather[0].description)
-                console.log(`   icone:`,item.weather[0].icon)
-                console.log(`   velocidade:`,item.wind.speed)
-                console.log(`   pais:`,data.city.country)
+        const dados = await getDados(x,y);
+        dadosClimaticos = []
+        
+        dadosClimaticos.push({
+            cidade: dados.city.name,
+            data: dados.list[0].dt_txt,
+            temperatura: dados.list[0].main.temp,
+            sensacao_termica:dados.list[0].main.feels_like,
+            umidade: dados.list[0].main.humidity,
+            clima: dados.list[0].weather[0].main,
+            clima_desc: dados.list[0].weather[0].description,
+            icon: dados.list[0].weather[0].icon,
+            velocidadeVento_M_S: dados.list[0].wind.speed,
+            pais: dados.city.country
+        });
+        dados.list.map((item) => {
+            if (item.dt_txt.split(' ')[1] === "18:00:00" && dados.list[0].dt_txt.split(' ')[0]!==item.dt_txt.split(' ')[0]) {
+                dadosClimaticos.push({
+                    data: item.dt_txt,
+                    temperatura: item.main.temp,
+                    sensacao_termica:item.main.feels_like,
+                    umidade: item.main.humidity,
+                    clima: item.weather[0].main,
+                    clima_desc: item.weather[0].description,
+                    icon: item.weather[0].icon,
+                    velocidadeVento_M_S: item.wind.speed,
+                    pais: dados.city.country
+                });
             }
         })
-     }
-MostraTempo()
+        console.log(dadosClimaticos)
+        return dadosClimaticos
+        
+}
+getDadosClima()
+
+
